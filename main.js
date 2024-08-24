4102aed062c81b2b2115f60123035f9e3f4c2e82
rich111ard <98288372+rich111ard@users.noreply.github.com>
2024-08-24 18:24:04 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
+})
+ 
+
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        //change icon
+         icon = faq.querySelector('.faq_icon i');
+         if(icon.className === 'uil uil-plus'){
+            icon.className = 'uil uil-minus';
+         }else{
+            icon.className = 'uil uil-plus'
+         }
+    })
+})
+
+
+// MENU items show/hide
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+})
+
+
+// close nav menu
+
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+}
+
+closeBtn.addEventListener('click', closeNav)
+
+
+
+document.addEventListener("scroll", function() {
+    var video = document.getElementById("vid");
+    
+    // Check if the video is within the viewport
+    var bounding = video.getBoundingClientRect();
+    var inViewport = bounding.top >= 0 && bounding.bottom <= window.innerHeight;
+
+    // Mute video if it's outside the viewport, unmute if inside
+    if (!inViewport) {
+        video.muted = true;
+    } 
+});
